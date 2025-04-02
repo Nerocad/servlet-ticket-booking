46a9992d768ad30060778bb32760c466c914ddd2
nerocad <unsober31@gmail.com>
2025-04-02 11:36:34 +0300

    Добавил тесты на AirportRepository

--- src/test/java/com/walking/tbooking/repository/AirportRepositoryTest.java
+++ src/test/java/com/walking/tbooking/repository/AirportRepositoryTest.java
@@ -6,6 +6,8 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -21,7 +23,8 @@ import static org.mockito.Mockito.*;
 
 
 public class AirportRepositoryTest {
-    private HikariDataSource dataSource;
+
+    private DataSource dataSource;
     private Connection connection;
     private PreparedStatement statement;
     private ResultSet rs;
@@ -43,18 +46,67 @@ public class AirportRepositoryTest {
     }
 
     @Test
-    void findById_WhenAirportExists_ReturnsAirport() throws SQLException {
-        Long id = 1L;
-        Airport expectedAirport = new Airport(id, "SVO", "Шереметьево", "Москва");
+    void findById_whenAirportExists() throws SQLException {
+
+        Airport expectedAirport = new Airport(1L, "SVO", "Шереметьево", "Москва");
 
         when(statement.executeQuery()).thenReturn(rs);
         when(rs.next()).thenReturn(true);
         when(converter.convert(rs)).thenReturn(Optional.of(expectedAirport));
 
-        Optional<Airport> result = airportRepository.findById(id);
+        Optional<Airport> result = airportRepository.findById(1L);
 
         assertTrue(result.isPresent());
         assertEquals(expectedAirport, result.get());
-        verify(statement).setLong(1, id);
+        verify(statement).setLong(1, 1L);
+    }
+    @Test
+    void findById_whenAirportNotExists() throws SQLException {
+        when(statement.executeQuery()).thenReturn(rs);
+        when(rs.next()).thenReturn(true);
+        when(converter.convert(rs)).thenReturn(Optional.empty());
+
+        Optional<Airport> result = airportRepository.findById(1L);
+
+        assertTrue(result.isEmpty());
+
+    }
+    @Test
+    void create_airport_thenReturns_airportWithId() throws SQLException {
+        Airport airport = new Airport(null, "SVO", "Шереметьево", "Москва");
+
+        when(statement.executeQuery()).thenReturn(rs);
+        when(rs.next()).thenReturn(true);
+        when(rs.getLong(1)).thenReturn(1L);
+
+        Airport createdAirport = airportRepository.create(airport);
+
+        assertEquals(1L, createdAirport.getId());
+        verify(statement).setString(1, "SVO");
+        verify(statement).setString(2, "Шереметьево");
+        verify(statement).setString(3, "Москва");
+    }
+    @Test
+    void update_airport() throws SQLException {
+        Airport airport = new Airport(1L, "SVO", "Шереметьево", "Москва");
+
+        airportRepository.update(airport);
+
+        verify(statement).setString(1,"SVO");
+        verify(statement).setString(2,"Шереметьево");
+        verify(statement).setString(3,"Москва");
+        verify(statement).setLong(4, 1L);
+        verify(statement).executeUpdate();
+
+    }
+    @Test
+    void delete_airport() throws SQLException {
+        airportRepository.deleteById(1L);
+
+        verify(statement).setLong(1, 1L);
+        verify(statement).executeUpdate();
     }
 }
+
+
+
